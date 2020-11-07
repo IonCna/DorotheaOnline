01c01229d7b9289c8231d06b988ad91159e626f0
TheSheeper <37430861+TheSheeper@users.noreply.github.com>
2020-11-07 12:48:49 -0600

    a

---
+++ App.js
@@ -0,0 +1,54 @@
+// Imports
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Static Files
+app.use(express.static("public"));
+app.use("/css", express.static(`${__dirname}/public/css`));
+app.use("/js", express.static(`${__dirname}/public/js`));
+app.use("/img", express.static(`${__dirname}/public/img`));
+
+app.use(express.static("src"));
+app.use("/src", express.static(`${__dirname}/src/`));
+
+// DEV ONLY
+app.use("/test", express.static(`${__dirname}/test/`));
+
+// Set Views
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/Index.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/Register.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/About.html");
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/views/Contact.html");
+});
+
+app.get("/changelog", (req, res) => {
+  res.sendFile(__dirname + "/Change.log");
+});
+
+app.get("/test", (req, res) => {
+  res.sendFile(__dirname + "/test/Test.html");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/views/404.html");
+});
+
+//Listen on...
+app.listen(port, () => console.info(`Listening on port ${port}`));
---
+++ src/Header.Gen.js
@@ -0,0 +1,46 @@
+(function () {
+  let header = `
+  <section class="header__container">
+    <div class="title__container">
+      <h1 id="title">
+        <p>Enfermería Dorothea E.</p>
+      </h1>
+      <h2>
+        <p>Orem.</p>
+      </h2>
+    </div>
+  </section>
+  `;
+
+  let subHeader = `
+  <section class="subHeader__container">
+    <div class="subHeader_menu__container">
+      <li class="menu_item">
+        <a href="/">INICIO</a>
+      </li>
+      <li class="menu_item">
+        <a href="about">QUIENES SOMOS</a>
+      </li>
+      <li class="menu_item">
+        <a href="/">MODELO EDUCATIVO</a>
+      </li>
+      <li class="menu_item">
+        <a href="/">PROGRAMA ACADEMICO</a>
+      </li>
+      <li class="menu_item">
+        <a href="login">PLATAFORMA</a>
+      </li>
+      <li class="menu_item">
+        <a href="contact">CONTACTO</a>
+      </li>
+      <li class="menu_item">
+        <a href="login">PAGO EN LINEA</a>
+      </li>
+    </div>
+  </section>
+  `;
+
+  let domElement = document.querySelector("header");
+
+  domElement.innerHTML = header + subHeader;
+})();

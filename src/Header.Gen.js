a07ee05613a126b0882c9353da31ccc493ed6866
Max Alejandro <globinbob@gmail.com>
2020-12-28 14:54:03 -0600

    Header mas HD

--- src/Header.Gen.js
+++ src/Header.Gen.js
@@ -1,13 +1,10 @@
 (function () {
   let header = `
   <section class="header__container">
-    <div class="title__container">
-      <h1 id="title">
-        <p>Escuela de Enfermería y Formacón para la Salud</p>
-      </h1>
-      <h2 id="subtitle">
-        Dorothea E. Orem
-      </h2>
+    <div class="title_logo__container">
+      <img src="img/logo.png" alt=".logo"/>
+    </div>
+    <div id="title" class="title__container">
     </div>
   </section>`;
 
@@ -31,10 +28,45 @@
   let btnSubHeader = document.getElementById("btn");
   btnSubHeader.onclick = () => displayMenu();
 
+  let title = document.getElementById("title");
+
+  window.innerWidth >= 1024
+  ? (title.innerHTML = getTitle(1))
+  : (title.innerHTML = getTitle(0));
+
   function displayMenu() {
     let x = document.getElementById("menuContainer");
-  
-    if (x.className === "subHeader_menu__container") x.className += " responsive";
+
+    if (x.className === "subHeader_menu__container")
+      x.className += " responsive";
     else x.className = "subHeader_menu__container";
   }
+
+  /**
+   * @param {number} mode
+   */
+
+  function getTitle(mode) {
+    if (mode === 1) {
+      return `
+      <h1>
+        Escuela de Enfermería para la Salud
+      </h1>
+      <h2>
+        Dorothea E. Orem.
+      </h2>
+      `;
+    } else
+      return `
+    <h1>
+      Dorothea E. Orem.
+    </h1>
+    `;
+  }
+
+  window.addEventListener("resize", () => {
+    window.innerWidth >= 1024
+      ? (title.innerHTML = getTitle(1))
+      : (title.innerHTML = getTitle(0));
+  });
 })();
